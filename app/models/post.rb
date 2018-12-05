8bed9bf4f752bcf7211b838ce1aaf791bbcee30d
Raymond K. Woods <raymondkwoods@hotmail.com>
2018-12-04 22:49:27 -0800

    assignment 9: add censor_posts method to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
 	has_many :comments
+	attr_accessor :censor_posts
+
+	def self.censor_posts()
+		self.all.each_with_index { |p, index| p.update_attributes!(title: 'SPAM') if index == 0 || ((index+1) % 5) == 0 }
+	end
 end
