0c64f64fcc560928117faf19e81c9ee9e5fd9c66
Raymond K. Woods <raymondkwoods@hotmail.com>
2018-12-07 20:29:30 -0800

    posts controller and view updates for new, create, and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,31 @@
 class PostsController < ApplicationController
-  def index
-  	@posts = Post.all
-  end
+	def index
+		@posts = Post.all
+	end
 
-  def show
-  end
+	def show
+		@post = Post.find(params[:id])
+	end
 
-  def new
-  end
+	def new
+		@post = Post.new
+	end
 
-  def edit
-  end
+	def create
+		@post = Post.new
+		@post.title = params[:post][:title]
+		@post.body = params[:post][:body]
+
+		if @post.save
+			flash[:notice] = "Post was saved."
+			redirect_to @post
+		else
+			flash.now[:alert] = "There was an error saving the post. Please try again."
+			render :new
+		end
+	end
+
+	def edit
+	end
+
 end
