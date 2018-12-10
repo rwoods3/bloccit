b9713990c9afeeb28fd2c4ca76d116f80d77067e
Raymond K. Woods <raymondkwoods@hotmail.com>
2018-12-10 02:01:39 -0800

    checkpoint 11: update and delete posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	has_many :comments
+	has_many :comments, dependent: :destroy
 
 	def self.censor_posts()
 		self.all.each_with_index { |p, index| p.update_attributes!(title: 'SPAM') if index == 0 || ((index+1) % 5) == 0 }
