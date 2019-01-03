5de233ce0e87b759492e23cf35547d6a20f3f195
Raymond K. Woods <raymondkwoods@hotmail.com>
2019-01-02 21:57:12 -0800

    checkpoint: validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
 	belongs_to :topic
 	has_many :comments, dependent: :destroy
 
+   	validates :title, length: { minimum: 5 }, presence: true
+   	validates :body, length: { minimum: 20 }, presence: true
+   	validates :topic, presence: true
+
 	def self.censor_posts()
 		self.all.each_with_index { |p, index| p.update_attributes!(title: 'SPAM') if index == 0 || ((index+1) % 5) == 0 }
 	end
