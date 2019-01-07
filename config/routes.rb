7d3babfcac02934b3dd420b1171b614cf82d5cce
Raymond K. Woods <raymondkwoods@hotmail.com>
2019-01-06 20:01:14 -0800

    checkpoint: sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :advertisements, except: [:edit, :update, :destroy]
   resources :questions
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   post 'users/confirm' => 'users#confirm'
 
