b9defe79321e1def8e55852d19d03346ec686de7
Raymond K. Woods <raymondkwoods@hotmail.com>
2019-01-08 22:59:40 -0800

    assignment: a few fixes that I missed in my first attempt 1. Added create and update actions 2. Cleaned up my sponsored posts form partial 3. Re-wrote controller to use strong params and mass assignment

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -1,6 +1,6 @@
 class SponsoredPostsController < ApplicationController
   def show
-  	@sponsored_post = SponsoredPost.find(params[:topic_id], params[:id])
+  	@sponsored_post = SponsoredPost.find(params[:id])
   end
 
   def new
@@ -8,7 +8,44 @@ class SponsoredPostsController < ApplicationController
   	@sponsored_post = SponsoredPost.create(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: RandomData.random_price)
   end
 
+  def create
+    @topic = Topic.find(params[:topic_id])
+    #@sponsored_post = SponsoredPost.create(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: RandomData.random_price)
+    @sponsored_post = SponsoredPost.new(sponsored_posts_params)
+
+    # @sponsored_post.title = params[:sponsored_post][:title]
+    # @sponsored_post.body = params[:sponsored_post][:body]
+    # @sponsored_post.body = params[:sponsored_post][:price]
+    @topic = Topic.find(params[:topic_id])
+    @sponsored_post.topic = @topic
+
+    if @sponsored_post.save
+      flash[:notice] = "Sponsored Post was saved."
+      redirect_to [@topic, @sponsored_post]
+    else
+      flash.now[:alert] = "There was an error saving the sponsored post. Please try again."
+      render :new
+    end
+  end
+
   def edit
-  	@sponsored_post = SponsoredPost.find(params[:topic_id], params[:id])
+  	@sponsored_post = SponsoredPost.find(params[:id])
+  end
+
+  def update
+    @sponsored_post = SponsoredPost.find(params[:id])
+
+    if @sponsored_post.update_attributes(sponsored_posts_params)
+      flash[:notice] = "Sponsored Post was updated."
+      redirect_to [@sponsored_post.topic, @sponsored_post]
+    else
+      flash.now[:alert] = "There was an error updating the sponsored post. Please try again."
+      render :edit
+    end
+  end
+
+  private
+  def sponsored_posts_params
+    params.require(:sponsored_post).permit(:title, :body, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :topics do
   	resources :posts, except: [:index]
-  	resources :sponsored_posts, only: [:show, :new, :edit, :create]
+  	resources :sponsored_posts, except: [:index]
   end
   resources :advertisements, except: [:edit, :update, :destroy]
   resources :questions
