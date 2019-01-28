1dfeca1ea79025c64db3d37003f2167b552ba987
Raymond K. Woods <raymondkwoods@hotmail.com>
2019-01-27 16:16:28 -0800

    assignment: profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,56 @@
 class UsersController < ApplicationController
-   def new
-     @user = User.new
-   end
+  def new
+   @user = User.new
+  end
 
-   def create
-   	@user = User.new
-   	@user.name = params[:user][:name]
-   	@user.email = params[:user][:email]
-   	@user.password = params[:user][:password]
-   	@user.password_confirmation = params[:user][:password_confirmation]
-   	
-     if @user.save
-       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-       create_session(@user)
-       redirect_to root_path
-     else
-       flash.now[:alert] = "There was an error creating your account. Please try again."
-       render :new
-     end
+  def create
+  	@user = User.new
+  	@user.name = params[:user][:name]
+  	@user.email = params[:user][:email]
+  	@user.password = params[:user][:password]
+  	@user.password_confirmation = params[:user][:password_confirmation]
+  	
+   if @user.save
+     flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+     create_session(@user)
+     redirect_to root_path
+   else
+     flash.now[:alert] = "There was an error creating your account. Please try again."
+     render :new
    end
+  end
 
-   def confirm
-   	@user = User.new
-   	@user.name = params[:user][:name]
-   	@user.email = params[:user][:email]
-   	@user.password = params[:user][:password]
-   	@user.password_confirmation = params[:user][:password_confirmation]
-   end
+  def confirm
+  	@user = User.new
+  	@user.name = params[:user][:name]
+  	@user.email = params[:user][:email]
+  	@user.password = params[:user][:password]
+  	@user.password_confirmation = params[:user][:password_confirmation]
+  end
 
-   def show
-     @user = User.find(params[:id])
-     @posts = @user.posts.visible_to(current_user)
-   end
+  def show
+   @user = User.find(params[:id])
+   @posts = @user.posts.visible_to(current_user)
+  end
 
-   def user_params
-   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-   end
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
-   helper_method :user_params
+  def has_posts
+    @user.posts.any?
+  end
+
+  def has_comments
+    @user.comments.any?
+  end
+
+  def has_favorites
+    @user.favorites.any?
+  end
+  
+  helper_method :user_params
+  helper_method :has_posts
+  helper_method :has_comments
+  helper_method :has_favorites
 end
